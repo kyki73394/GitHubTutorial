1c93e3e3d911a4c89051bfd7aa89f2a7f9c8ef73
Hannah Kim <kyki73394@gmail.com>
2024-03-14 14:20:50 -0400

    Initial Commit

---
+++ GitHubTutorial/GitHubTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubTutorialApp.swift
+//  GitHubTutorial
+//
+//  Created by Scholar on 3/14/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHubTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
